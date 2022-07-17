9512a1cf2e3d5d0679ba802ca15bb13314d68b86
AritaGuy <arita.sam@student.moringaschool.com>
2022-07-17 15:08:29 +0300

    created leap year array and function

--- inde.js
+++ inde.js
@@ -44,4 +44,64 @@ let calc = function(a,b){
 };
 calc(selling_price, time);
 
+// Creating yearly object
+let jan ={
+    name : "January",
+    days : 31 
+};
+let feb ={
+    name : "February",
+    days : 29 
+};
+let mar={
+    name : "March",
+    days : 31
+};
+let apr={
+    name : "April",
+    days : 30 
+};
+let may={
+    name : "May",
+    days : 31
+};
+let jun={
+    name : "March",
+    days : 30
+};
+let jul={
+    name : "July",
+    days : 31
+};
+let aug={
+    name : "August",
+    days : 31
+};
+let sept={
+    name : "Sept",
+    days : 30
+};
+let oct={
+    name : "October",
+    days : 31
+};
+let nov={
+    name : "November",
+    days : 30
+};
+let dec={
+    name : "December",
+    days : 31
+};
 
+//leap year array
+let yearArray = [jan,feb,mar,apr,may,jun,jul,aug,sept,oct,nov,dec]
+
+//Leap year function
+let lpyr = function leapyear(){
+    let yr = totalProd * yearArray[i].days * 45;
+    for(i=0; i<yearArray.length; i++){
+        console.log(`Your income for ${yearArray[i].name} is ${yr}`)
+    };
+};
+lpyr()
