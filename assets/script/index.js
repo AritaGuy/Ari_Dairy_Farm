d7183c8c6ec45faf03f364cd412df5e6959b4927
AritaGuy <arita.sam@student.moringaschool.com>
2022-07-18 03:30:26 +0300

    Created per shed object

---
+++ assets/script/index.js
@@ -0,0 +1,17 @@
+// Creating object
+let a ={
+    name : "Shed A",
+    volume : 510 
+};
+let b ={
+    name : "Shed B",
+    volume : 308 
+};
+let c={
+    name : "Shed C",
+    volume : 486 
+};
+let d={
+    name : "Shed D",
+    volume : 572 
+};
